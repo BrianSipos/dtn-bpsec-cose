8b289bee2e28960c59de5c708294bd692809613b
Brian Sipos <bsipos@rkf-eng.com>
2021-03-16 20:12:54 -0400

    Tightening message and PKIX requirements

---
+++ src/bpsec_cose/test/test_sign1_eddsa.py
@@ -0,0 +1,97 @@
+import binascii
+import cbor2
+from cose import curves, headers, algorithms
+from cose.keys import OKPKey, keyops, keyparam
+from cose.messages import Sign1Message
+from ..util import encode_diagnostic
+from ..bpsec import BlockType
+from .base import BaseTest
+
+
+class TestExample(BaseTest):
+
+    def test(self):
+        print('\nTest: ' + __name__ + '.' + type(self).__name__)
+        private_key = OKPKey(
+            crv=curves.Ed25519,
+            x=binascii.unhexlify('64f38ea84b153c7be87349f78261ca46e90f1613a3ceb2ae02c010193631e07d'),
+            d=binascii.unhexlify('6820977a9be08d676dac7ee19e1595d0552894ee2d71feb1d7b1d2a9f31754fd'),
+            optional_params={
+                keyparam.KpKid: b'ExampleEd25519',
+                keyparam.KpKeyOps: [keyops.SignOp, keyops.VerifyOp],
+            }
+        )
+        print('Private Key: {}'.format(encode_diagnostic(cbor2.loads(private_key.encode()))))
+
+        # Primary block
+        prim_dec = self._get_primary_item()
+        prim_enc = cbor2.dumps(prim_dec)
+        print('Primary Block: {}'.format(encode_diagnostic(prim_dec)))
+        print('Encoded: {}'.format(encode_diagnostic(prim_enc)))
+
+        # Security target block
+        target_dec = self._get_target_item()
+        content_plaintext = target_dec[4]
+        print('Target Block: {}'.format(encode_diagnostic(target_dec)))
+        print('Plaintext: {}'.format(encode_diagnostic(content_plaintext)))
+
+        # Combined AAD
+        ext_aad_dec = self._get_aad_item()
+        ext_aad_enc = cbor2.dumps(ext_aad_dec)
+        print('External AAD: {}'.format(encode_diagnostic(ext_aad_dec)))
+        print('Encoded: {}'.format(encode_diagnostic(ext_aad_enc)))
+
+        msg_obj = Sign1Message(
+            phdr={
+                headers.Algorithm: algorithms.EdDSA,
+            },
+            uhdr={
+                headers.KID: private_key.kid,
+            },
+            payload=content_plaintext,
+            # Non-encoded parameters
+            external_aad=ext_aad_enc,
+        )
+        msg_obj.key = private_key
+
+        # COSE internal structure
+        cose_struct_enc = msg_obj._sig_structure
+        cose_struct_dec = cbor2.loads(cose_struct_enc)
+        print('COSE Structure: {}'.format(encode_diagnostic(cose_struct_dec)))
+        print('Encoded: {}'.format(encode_diagnostic(cose_struct_enc)))
+
+        # Encoded message
+        message_enc = msg_obj.encode(tag=False)
+        message_dec = cbor2.loads(message_enc)
+        # Detach the payload
+        content_signature = message_dec[2]
+        message_dec[2] = None
+        self._print_message(message_dec, recipient_idx=4)
+        message_enc = cbor2.dumps(message_dec)
+
+        # ASB structure
+        asb_dec = self._get_asb_item([
+            msg_obj.cbor_tag,
+            message_enc
+        ])
+        asb_enc = cbor2.dumps(asb_dec)
+        print('ASB: {}'.format(encode_diagnostic(asb_dec)))
+        print('Encoded: {}'.format(encode_diagnostic(asb_enc)))
+
+        bpsec_dec = self._get_bpsec_item(
+            block_type=BlockType.BIB,
+            asb_dec=asb_dec,
+        )
+        bpsec_enc = cbor2.dumps(bpsec_dec)
+        print('BPSec block: {}'.format(encode_diagnostic(bpsec_dec)))
+        print('Encoded: {}'.format(encode_diagnostic(bpsec_enc)))
+
+        # Change from detached payload
+        message_dec[2] = content_signature
+        decode_obj = Sign1Message.from_cose_obj(message_dec)
+        decode_obj.external_aad = ext_aad_enc
+        decode_obj.key = private_key
+
+        verify_valid = decode_obj.verify_signature()
+        self.assertTrue(verify_valid)
+        print('Loopback verify:', verify_valid)
