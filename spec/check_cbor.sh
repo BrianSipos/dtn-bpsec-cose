77e345bac32f528f602c3163052ba703845d8350
Brian Sipos <brian.sipos@jhuapl.edu>
2025-01-16 12:27:22 -0500

    Reconciling build config

--- spec/check_cbor.sh
+++ spec/check_cbor.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # Read stdin and verify it as valid CBOR against a CDDL schema.
 set -e
-set -o pipefail
-
 SELFDIR=$(readlink -f $(dirname "${BASH_SOURCE[0]}"))
 
 COMBINED_CDDL=$1
 
-cat | diag2cbor.rb | cddl validate --cddl "${COMBINED_CDDL}" --stdin
+TMPFILE=$(mktemp)
+cat | diag2cbor.rb >${TMPFILE}
+cddl validate --cddl "${COMBINED_CDDL}" --cbor ${TMPFILE}
+rm ${TMPFILE}
