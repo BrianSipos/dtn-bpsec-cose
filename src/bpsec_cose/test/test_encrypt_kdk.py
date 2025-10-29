683dd55317ae97e4d6ef420c6a45694c809518e2
Brian Sipos <brian.sipos@jhuapl.edu>
2025-10-29 11:14:37 -0400

    Make explicit examples using KW vs KDF

--- src/bpsec_cose/test/test_encrypt_kdk.py
+++ src/bpsec_cose/test/test_encrypt_kdk.py
@@ -1,11 +1,9 @@
 import binascii
 import cbor2
-from cryptography.hazmat.primitives.asymmetric import ec
-from cryptography.hazmat.primitives import serialization
 from pycose import headers, algorithms
-from pycose.keys import SymmetricKey, EC2Key, curves, keyops, keyparam
+from pycose.keys import SymmetricKey, keyops, keyparam
 from pycose.messages import EncMessage
-from pycose.messages.recipient import KeyAgreementWithKeyWrap
+from pycose.messages.recipient import DirectEncryption
 from ..util import dump_cborseq, encode_diagnostic
 from ..bpsec import BlockType
 from .base import BaseTest
@@ -15,72 +13,22 @@ class TestExample(BaseTest):
 
     def test(self):
         print('\nTest: ' + __name__ + '.' + type(self).__name__)
-        recipient_key = EC2Key(
-            crv=curves.P256,
-            x=binascii.unhexlify('44c1fa63b84f172b50541339c50beb0e630241ecb4eebbddb8b5e4fe0a1787a8'),
-            y=binascii.unhexlify('059451c7630d95d0b550acbd02e979b3f4f74e645b74715fafbc1639960a0c7a'),
-            d=binascii.unhexlify('dd6e7d8c4c0e0c0bd3ae1b4a2fa86b9a09b7efee4a233772cf5189786ea63842'),
+        # 256-bit key derivation key
+        kdk = SymmetricKey(
+            k=binascii.unhexlify('0E8A982B921D1086241798032FEDC1F883EAB72E4E43BB2D11CFAE38AD7A972E'),
             optional_params={
-                keyparam.KpKid: b'ExampleEC2',
-                keyparam.KpAlg: algorithms.EcdhEsA256KW,
+                keyparam.KpKid: b'ExampleKEK',
+                keyparam.KpAlg: algorithms.DirectHKDFSHA512,
                 keyparam.KpKeyOps: [keyops.DeriveKeyOp],
             }
         )
-        print('Private Key: {}'.format(encode_diagnostic(cbor2.loads(recipient_key.encode()))))
-        recipient_public = EC2Key(
-            crv=recipient_key.crv,
-            x=recipient_key.x,
-            y=recipient_key.y
-        )
+        print('KDK: {}'.format(encode_diagnostic(cbor2.loads(kdk.encode()))))
+        kdf_salt = binascii.unhexlify('2fa8c8352aea17faf7407271a5e90eb8')
+        print('KDF salt: {}'.format(binascii.hexlify(kdf_salt)))
 
-        ckey = ec.EllipticCurvePrivateNumbers(
-            int.from_bytes(recipient_key.d, 'big'),
-            ec.EllipticCurvePublicNumbers(
-                x=int.from_bytes(recipient_key.x, 'big'),
-                y=int.from_bytes(recipient_key.y, 'big'),
-                curve=recipient_key.crv.curve_obj
-            )
-        ).private_key()
-        ckey_pem = ckey.private_bytes(
-            encoding=serialization.Encoding.PEM,
-            format=serialization.PrivateFormat.TraditionalOpenSSL,
-            encryption_algorithm=serialization.NoEncryption(),
-        )
-        print('Private PEM:\n{}'.format(ckey_pem.decode('utf-8')))
-        serialization.load_pem_private_key(ckey_pem, password=None)
-
-        # 256-bit content encryption key
-        cek = SymmetricKey(
-            k=binascii.unhexlify('13BF9CEAD057C0ACA2C9E52471CA4B19DDFAF4C0784E3F3E8E3999DBAE4CE45C'),
-            optional_params={
-                keyparam.KpKid: b'ExampleCEK',
-                keyparam.KpAlg: algorithms.A256GCM,
-                keyparam.KpKeyOps: [keyops.EncryptOp, keyops.DecryptOp],
-            }
-        )
-        print('CEK: {}'.format(encode_diagnostic(cbor2.loads(cek.encode()))))
         # session IV
         iv = binascii.unhexlify('6F3093EBA5D85143C3DC484A')
         print('IV: {}'.format(binascii.hexlify(iv)))
-
-        # Would be random ephemeral key, but test constant
-        sender_key = EC2Key(
-            crv=curves.P256,
-            x=binascii.unhexlify('fedaba748882050d1bef8ba992911898f554450952070aeb4788ca57d1df6bcc'),
-            y=binascii.unhexlify('ceaa8e7ff4751a4f81c70e98f1713378b0bd82a1414a2f493c1c9c0670f28d62'),
-            d=binascii.unhexlify('a2e4ed4f2e21842999b0e9ebdaad7465efd5c29bd5761f5c20880f9d9c3b122a'),
-            optional_params={
-                keyparam.KpKid: b'SenderEC2',
-                keyparam.KpAlg: algorithms.EcdhEsA256KW,
-                keyparam.KpKeyOps: [keyops.DeriveKeyOp],
-            }
-        )
-        print('Sender Private Key: {}'.format(encode_diagnostic(cbor2.loads(sender_key.encode()))))
-        sender_public = EC2Key(
-            crv=sender_key.crv,
-            x=sender_key.x,
-            y=sender_key.y
-        )
 
         # Primary block
         prim_dec = self._get_primary_item()
@@ -102,32 +50,27 @@ class TestExample(BaseTest):
 
         msg_obj = EncMessage(
             phdr={
-                headers.Algorithm: cek.alg,
+                headers.Algorithm: algorithms.A256GCM,
             },
             uhdr={
                 headers.IV: iv,
             },
             payload=content_plaintext,
             recipients=[
-                KeyAgreementWithKeyWrap(
+                DirectEncryption(
                     phdr={
-                        headers.Algorithm: recipient_key.alg,
+                        headers.Algorithm: kdk.alg,
                     },
                     uhdr={
-                        headers.KID: recipient_key.kid,
-                        headers.EphemeralKey: sender_public,
+                        headers.KID: kdk.kid,
+                        headers.Salt: kdf_salt,
                     },
-                    payload=cek.k,
+                    key=kdk,
                 ),
             ],
             # Non-encoded parameters
             external_aad=ext_aad_enc,
         )
-        recip = msg_obj.recipients[0]
-        recip.key = sender_key
-        recip.local_attrs = {
-            headers.StaticKey: recipient_public,
-        }
 
         # COSE internal structure
         cose_struct_enc = msg_obj._enc_structure
@@ -168,13 +111,13 @@ class TestExample(BaseTest):
         decode_obj.external_aad = ext_aad_enc
 
         recip = decode_obj.recipients[0]
-        recip.key = recipient_key
+        recip.key = kdk
         decode_plaintext = decode_obj.decrypt(recipient=recip)
         print('Loopback plaintext:', encode_diagnostic(decode_plaintext))
         self.assertEqual(content_plaintext, decode_plaintext)
 
         print('Loopback CEK:', encode_diagnostic(cbor2.loads(decode_obj.key.encode())))
-        self.assertEqual(cek.k, decode_obj.key.k)
+        self.assertIsNotNone(decode_obj.key.k)
 
         target_dec[4] = content_ciphertext
         target_enc = cbor2.dumps(target_dec)
