f0ea9ab40acc96fc6cef852a1fca2587cd645fa1
Brian Sipos <brian.sipos@jhuapl.edu>
2026-08-03 11:36:51 -0400

    Add example with additional protected headers (#135)

---
+++ src/bpsec_cose/test/test_mac0_addlprot.py
@@ -0,0 +1,107 @@
+import cbor2
+from pycose import headers, algorithms
+from pycose.keys import SymmetricKey, keyops, keyparam
+from pycose.messages import Mac0Message
+from ..util import dump_cborseq, cbor2diag
+from ..bpsec import BlockType
+from .base import BaseTest
+
+KEY = SymmetricKey(
+    k=bytes.fromhex('3a5c74e32ab4558a99581ec3a816576812aabe895db04494cda25b711d7b5ed4077466e677860648412f1bf8c91d0624'),
+    optional_params={
+        keyparam.KpKid: b'ExampleA.1',
+        keyparam.KpAlg: algorithms.HMAC384,
+        keyparam.KpKeyOps: [keyops.MacCreateOp, keyops.MacVerifyOp],
+    }
+)
+""" 384-bit key (SHA-384 output size) """
+
+
+class TestExample(BaseTest):
+
+    _ADDL_PROT = {
+        headers.KID.identifier: KEY.kid,
+    }
+    _ADDL_PROT_ENC = cbor2.dumps(_ADDL_PROT)
+
+    def test(self):
+        self._logger.info('Key: %s', cbor2diag(KEY.encode()))
+
+        # Primary block
+        prim_dec = self._get_primary_item()
+        prim_enc = cbor2.dumps(prim_dec)
+        self._logger.info('Primary Block: %s', cbor2diag(prim_enc))
+        self._logger.info('Encoded: %s', prim_enc.hex())
+
+        # Security target block
+        target_dec = self._get_target_item()
+        content_plaintext = target_dec[4]
+        self._logger.info('Target Block: %s', cbor2diag(cbor2.dumps(target_dec)))
+        self._logger.info('Plaintext: %s', content_plaintext.hex())
+
+        # Additional header maps
+        self._logger.info('Additional Protected: %s', cbor2diag(self._ADDL_PROT_ENC))
+        self._logger.info('Encoded: %s', self._ADDL_PROT_ENC.hex())
+
+        # Combined AAD
+        ext_aad_dec = self._get_aad_array()
+        ext_aad_enc = dump_cborseq(ext_aad_dec)
+        self._logger.info('External AAD: %s', cbor2diag(ext_aad_enc))
+        self._logger.info('Encoded: %s', ext_aad_enc.hex())
+
+        msg_obj = Mac0Message(
+            phdr={
+                headers.Algorithm: KEY.alg,
+            },
+            uhdr={
+            },
+            payload=content_plaintext,
+            # Non-encoded parameters
+            external_aad=ext_aad_enc,
+        )
+        msg_obj.key = KEY
+
+        # COSE internal structure
+        cose_struct_enc = msg_obj._mac_structure
+        self._logger.info('COSE Structure: %s', cbor2diag(cose_struct_enc))
+        self._logger.info('Encoded: %s', cose_struct_enc.hex())
+
+        # Encoded message
+        message_enc = msg_obj.encode(tag=False)
+        message_dec = cbor2.loads(message_enc)
+        # Detach the payload
+        content_signature = message_dec[2]
+        message_dec[2] = None
+        self._print_message(message_dec, recipient_idx=4)
+        message_enc = cbor2.dumps(message_dec)
+
+        # ASB structure
+        asb_dec = self._get_asb_item((
+            msg_obj.cbor_tag,
+            message_enc
+        ))
+        asb_enc = self._get_asb_enc(asb_dec)
+        self._logger.info('ASB: %s', cbor2diag(asb_enc))
+        self._logger.info('Encoded: %s', asb_enc.hex())
+
+        bpsec_dec = self._get_bpsec_item(
+            block_type=BlockType.BIB,
+            asb_dec=asb_dec,
+        )
+        bpsec_enc = cbor2.dumps(bpsec_dec)
+        self._logger.info('BPSec block: %s', cbor2diag(bpsec_enc))
+        self._logger.info('Encoded: %s', bpsec_enc.hex())
+
+        # Change from detached payload
+        message_dec[2] = content_signature
+        decode_obj = Mac0Message.from_cose_obj(message_dec, allow_unknown_attributes=False)
+        decode_obj.external_aad = ext_aad_enc
+        decode_obj.key = KEY
+
+        verify_valid = decode_obj.verify_tag()
+        self.assertTrue(verify_valid)
+        self._logger.info('Loopback verify: %s', verify_valid)
+
+        target_enc = cbor2.dumps(target_dec)
+        bundle = self._assemble_bundle([prim_enc, bpsec_enc, target_enc])
+        self._print_bundle(bundle)
